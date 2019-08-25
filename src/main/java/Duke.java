b37684005163d17bfc9bed2e44b20baebf33776d
koushireo <koushireo@gmail.com>
2019-08-25 16:19:53 +0800

    level 01

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,23 @@
+/**
+ * My <b>class</b>.
+ * @see Duke
+ */
+
+import java.util.Scanner;
+
 public class Duke {
+    /** Not a javadoc (ignored). */
+
+    /**
+     * Doubles the value.
+     * The long and detailed explanation what the method does.
+     *
+     * @param value for doubling.
+     * @return double value.
+     */
+
+    /** Extra javadoc (ignored). */
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +25,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        Scanner a = new Scanner(System.in);
+        String response = a.next();
+        while (true) {
+            System.out.println("____________________________________________________________");
+            System.out.println(response);
+            System.out.println("____________________________________________________________");
+            response = a.next();
+            if (response.equals("bye")) {
+                break;
+            }
+        }
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye! Hope to see you again.");
+        System.out.println("____________________________________________________________");
+
     }
 }
